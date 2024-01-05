840b67d13fcd3c03161295f87a104b043582cd6e
joseph <nwekejoseph52@gmail.com>
2024-01-05 11:08:11 -0800

    updates

---
+++ src/app/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, NgbModalModule],
+})
+export class SharedModule {}
