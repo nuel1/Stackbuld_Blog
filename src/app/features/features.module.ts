036064c2e9eb7064ce3daa215b977a0190981c59
joseph <nwekejoseph52@gmail.com>
2024-01-06 21:08:27 -0800

    added pagination

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,10 +4,11 @@ import { BlogViewComponent } from './blog-view/blog-view.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [BlogViewComponent, BlogsComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule, NgxPaginationModule],
   exports: [SharedModule],
 })
 export class FeaturesModule {}
